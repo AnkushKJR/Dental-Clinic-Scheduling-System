c9644e40328ee68a8d7db4ae78d7cb094982b3cf
Ankush <akanjikar999@gmail.com>
2026-08-12 23:37:23 +0530

    project setup

---
+++ dental-clinic-scheduler/src/main/java/com/smile/dentalclinic/DentalClinicSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.smile.dentalclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalClinicSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalClinicSchedulerApplication.class, args);
+	}
+
+}
---
+++ dental-clinic-scheduler/src/test/java/com/smile/dentalclinic/DentalClinicSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smile.dentalclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalClinicSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
